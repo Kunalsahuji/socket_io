d066b89b3b0aa1ee55b5b9b08a79abc35e246cf2
Kunalsahuji <ksahu0103@gmail.com>
2024-06-11 20:27:57 +0530

    socketio setup

---
+++ socketapi.js
@@ -0,0 +1,19 @@
+const io = require("socket.io")();
+const socketapi = {
+    io: io
+};
+
+// Add your socket.io logic here!
+io.on("connection", function (socket) {
+    console.log("A user connected");
+
+    io.emit("max", "hello from server")
+});
+// end of socket.io logic
+
+module.exports = socketapi;
+
+// io : server
+// socket: single user/ client
+// on: recieve msg
+// emit: send msg
